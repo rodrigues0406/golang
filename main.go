4b788a6409aae15ce9475aece88785e88ba3bd1e
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-03-28 08:42:35 -0300

    operações

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main(){
-a, b :=10, 3 
-fmt.Println("A soma é:", a + b)
-fmt.Println(" A subtração é:", a - b)
-fmt.Println("A multiplicação é:", a * b)
-fmt.Println("A divisão é:", a / b)
-fmt.Println("O resto da divisão é:",a % b)
-a++
-fmt.Println("incrementar a", a)
-if a > 0 |&& b > 0 {
-	fmt.Println("Numeros Positivos")
-}
+
+var numero1 int
+var numero2 int 
+fmt.Println("digite um numero:")
+fmt.Scan(&numero1)
+fmt.Println("digite um numero:")
+fmt.Scan(&numero2)
+
+fmt.Println("A soma é:", numero1 + numero2)
+fmt.Println(" A subtração é:", numero1 - numero2)
+fmt.Println("A multiplicação é:", numero1 * numero2)
+fmt.Println("A divisão é:", numero1 / numero2)
+fmt.Println("O resto da divisão é:", numero1 % numero2)
+
 }

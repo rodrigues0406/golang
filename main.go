1c51a1cfa47964116c5573407322124d7588a371
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-03-28 08:19:15 -0300

    operadores logicos

--- main.go
+++ main.go
@@ -9,4 +9,9 @@ fmt.Println(" A subtração é:", a - b)
 fmt.Println("A multiplicação é:", a * b)
 fmt.Println("A divisão é:", a / b)
 fmt.Println("O resto da divisão é:",a % b)
+a++
+fmt.Println("incrementar a", a)
+if a > 0 |&& b > 0 {
+	fmt.Println("Numeros Positivos")
+}
 }

21228a53e5c6cf1343c42142e9939136f095d8fe
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-04 07:58:14 -0300

    super-heroi

--- main.go
+++ main.go
@@ -13,5 +13,16 @@ package main
 	fmt.Println(names)
 	names[3] = "Jujuu"
 	fmt.Println(names)
+	//Slice
+	var score = []int{10,20,30,40}
+	fmt.Println(score)
+	score[1] = 2 
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
 
-}
+	var superherois = [] string {"homem-aranha", "homem de ferro", "hulk"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "laterna verde", "flash")
+	fmt.Println(superherois, len(superherois), cap(superherois))
+ }

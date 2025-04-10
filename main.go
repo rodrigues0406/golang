f73f2ab61d83777a4c4d92dcca6120d256f5d7b5
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-10 08:22:21 -0300

    vetor numeros

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
-package main 
+package main
 
 import (
-      "fmt"
-      "strings" 
-	  "sort"
-    )
-
+	"fmt"
+)
 
 func main() {
-    greeting := "Hello my friends"
-    fmt.Println(strings.Contains(greeting, "dog"))
-    fmt.Println(strings.ReplaceAll(greeting, "my", "mine"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.ToLower(greeting))
-	fmt.Println(strings.Index(greeting, "my"))
-    fmt.Println(strings.Split(greeting, "!"))
-	ages := []int {50, 80, 10}
-	sort.Ints(ages)
-	fmt.Println(ages)
-	index := sort.SearchInts(ages, 50)
-	fmt.Println(index)
-	names := []string{"Julia", "Maria", "Rodrigues"}
-	sort.Strings(names)
-	fmt.Println(names)
-	fmt.Println(sort.SearchStrings(names, "Julia"))
+	var numeros [5]int
+	var soma int
 
-	
+	//slice
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Digite o  numero %d: ", i+1)
+		fmt.Scanln(&numeros[i])
+		soma += numeros[i]
+	}
 
- }
+	//slice
+	fmt.Printf("A soma é: %d\n", soma)
+}
 
  

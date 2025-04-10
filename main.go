a193514673a2b0e5456862997202c451216d29ad
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-10 13:07:48 -0300

    saldo

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    var idade int
+	var saldo int
+	var comando string
+	var valor int
 
-    fmt.Print("Digite sua idade: ")
-     fmt.Scan(&idade)
+	fmt.Println("Digite seu saldo inicial: ")
+	fmt.Scanln(&saldo)
 
+	fmt.Println("saldo atual:", saldo)
+	fmt.Println(" deseja sacar ou depositar? sacar/depositar")
+	fmt.Scanln(&comando)
+	if comando == "sacar" {
+		fmt.Println("digite o valor que irá sacar")
+		fmt.Scanln(&valor)
+		if valor > saldo {
+			fmt.Println("saldo indisponivel", saldo)
+		} else {
+			saldo -= valor
+			fmt.Println("saque realizado com sucesso!", valor, "seu saldo atual agora é", saldo)
+		}
+	} else if comando == "depositar" {
+		fmt.Println("digite o valor que irá depositar:")
+		fmt.Scan(&valor)
+		saldo += valor
+		fmt.Println("deposito realizado com sucesso, seu saldo é", saldo)
 
-    if idade < 18 {
-        fmt.Println("Você é menor de idade.")
-    } else if idade <= 60 {
-        fmt.Println("Você é adulto.")
-    } else {
-        fmt.Println("Você é idoso.")
-    }
+	}
+
 }
-
- 

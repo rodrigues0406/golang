6464f0426717150281612c0e8eebe5e25a4d5f27
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-10 09:39:03 -0300

    break

--- main.go
+++ main.go
@@ -5,18 +5,37 @@ import (
 )
 
 func main() {
-	var numeros [5]int
-	var soma int
+age := 45
+fmt.Println(age <= 50)
+fmt.Println(age >= 50)
+fmt.Println(age == 50)
+fmt.Println(age != 50)
 
-	//slice
-	for i := 0; i < 5; i++ {
-		fmt.Printf("Digite o  numero %d: ", i+1)
-		fmt.Scanln(&numeros[i])
-		soma += numeros[i]
-	}
+if age < 50 {
+	fmt.Println("menor que 30 anos ")
+} else if age < 40 {
+	fmt.Println(" menor que 40 anos ")
+} else {
+  fmt.Println("não é menor que 40 anos")
+}
 
-	//slice
-	fmt.Printf("A soma é: %d\n", soma)
+names := []string{"julia", "maria", "silva", "rodrigues"}
+
+for index, value := range names {
+	if index == 1 {
+	fmt.Println("Continue após a posição", index, "é valor", value)
+	continue 
+
+}
+if index > 2 {
+	fmt.Println("sair apos ", index)
+	break
+}
+
+	fmt.Println("valor: ", value)
+
+
+}
 }
 
  

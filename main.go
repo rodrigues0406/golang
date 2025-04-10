0185347633d864120493a6452476e15fefb708d1
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-10 10:31:46 -0300

    condicional

--- main.go
+++ main.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-age := 45
-fmt.Println(age <= 50)
-fmt.Println(age >= 50)
-fmt.Println(age == 50)
-fmt.Println(age != 50)
+    var idade int
 
-if age < 50 {
-	fmt.Println("menor que 30 anos ")
-} else if age < 40 {
-	fmt.Println(" menor que 40 anos ")
-} else {
-  fmt.Println("não é menor que 40 anos")
-}
-
-names := []string{"julia", "maria", "silva", "rodrigues"}
-
-for index, value := range names {
-	if index == 1 {
-	fmt.Println("Continue após a posição", index, "é valor", value)
-	continue 
-
-}
-if index > 2 {
-	fmt.Println("sair apos ", index)
-	break
-}
-
-	fmt.Println("valor: ", value)
+    fmt.Print("Digite sua idade: ")
+     fmt.Scan(&idade)
 
 
-}
+    if idade < 18 {
+        fmt.Println("Você é menor de idade.")
+    } else if idade <= 60 {
+        fmt.Println("Você é adulto.")
+    } else {
+        fmt.Println("Você é idoso.")
+    }
 }
 
  

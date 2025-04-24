95acb6637a7cfedd0fd056b0e9c0d089f1b73797
Aluno Dev25 <julia.m.rodrigues9@aluno.senai.br>
2025-04-24 13:24:43 -0300

    nome e sobrenome

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
-package main
+package main 
 
-import (
-	"fmt"
-)
-func main() {
-	alunoIdade :=  make(map[string]int)
-    alunoIdade["Julia"] = 16
-    alunoIdade["Isadora"] = 16
-    alunoIdade["Isabelly"] = 16
-    alunoIdade["Manu"] = 15
-    fmt.Println("Idade da Julia", alunoIdade["Julia"])
-    notasAlunos := map[string]float64{
-        "Bruno" : 9.7,
-        "Otávio" : 10,
-        "Fabiano" : 8.7,
-        "Isabelly" :9.5,
-    
-    }
-    for nome,nota := range notasAlunos{
-        fmt.Printf("%s tirou a nota %.1f\n", nome,nota)
-    }
+import "fmt"  
+
+func pegarNome() (string, string){
+    return "Barry", "Allen"
+
 }
 
-
+func main(){
+    nome, sobrenome := pegarNome()
+    fmt.Println(nome)
+    fmt.Println(sobrenome)
+}
 
